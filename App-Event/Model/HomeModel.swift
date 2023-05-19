e78889cb84ec014f0f9b47fdebef3aaf86f374e4
Gabriel Mors <gabrielmors210@gmail.com>
2023-05-18 23:34:20 -0400

    criando um pod e fazendo pequenas alteracoes

--- App-Event/Model/HomeModel.swift
+++ App-Event/Model/HomeModel.swift
@@ -9,21 +9,7 @@ struct Evento: Codable {
     let image: String
     let longitude, latitude, price: Double
     let title, id: String
-//
-//    init(people: [People], date: Int, description: String, image: String, longitude: Double, latitude: Double, price: Double, title: String, id: String) {
-//        self.people = people
-//        self.date = date
-//        self.description = description
-//        self.image = image
-//        self.longitude = longitude
-//        self.latitude = latitude
-//        self.price = price
-//        self.title = title
-//        self.id = id
-//   }
 }
-
-
 
 class People: Codable {
     
